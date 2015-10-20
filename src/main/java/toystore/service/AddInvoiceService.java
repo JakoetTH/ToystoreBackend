557ff8e296f4eb6a3944c0a6f9a1b428c0d84570
JakoetTH <thawhirwow@gmail.com>
2015-10-20 23:06:30 +0200

    last fix for the night hopefully...

--- src/main/java/toystore/service/AddInvoiceService.java
+++ src/main/java/toystore/service/AddInvoiceService.java
@@ -3,6 +3,7 @@ package toystore.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import toystore.conf.InvoiceFactory;
@@ -29,6 +30,7 @@ public class AddInvoiceService implements AddInvoiceDetails{
     private Customer customer;
     private Invoice invoice;
     private List<Orderline> orderlines;
+    private List<Orderline> orderlinesblank;
     @Override
     public Long addInvoice(Long customerID, Long orderID)
     {
@@ -39,12 +41,26 @@ public class AddInvoiceService implements AddInvoiceDetails{
         order = orderRepository.findOne(orderID);
         if(order==null || order.getCheckout())
             return null;
-
+        orderlinesblank = new ArrayList<Orderline>();
         orderlines = order.getOrderlines();
+        List<Orderline> orderlinesi = new ArrayList<Orderline>();
+        for(Orderline orderlineit: orderlines)
+            orderlinesi.add(orderlineit);
 
-        invoice = InvoiceFactory.createInvoice(orderID, order.getTotalPrice(), orderlines);
+        invoice = InvoiceFactory.createInvoice(orderID, order.getTotalPrice(), orderlinesblank);
         invoiceRepository.save(invoice);
+
         id = invoice.getID();
+
+        invoice = invoiceRepository.findOne(id);
+
+        invoice = new Invoice
+                        .Builder(invoice.getOrderID())
+                        .copy(invoice)
+                        .orderlines(orderlinesi)
+                        .build();
+
+        invoiceRepository.save(invoice);
 
         List<Invoice> invoices = customer.getInvoices();
         invoices.add(invoice);
